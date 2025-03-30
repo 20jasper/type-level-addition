91ef53c9e2e3d086edeff23f6fb3fdec25ef7d1a
Jacob Asper <jacobasper191@gmail.com>
2025-03-29 23:01:02 -0400

    refactor some stuffs

--- add.ts
+++ add.ts
@@ -9,7 +9,7 @@ namespace AddDigits {
     ...TupleLengthN<N>
   ]["length"];
 
-  type aaaaa = AddDigits<999, 990>;
+  type example = AddDigits<999, 990>;
   //     ^?
 }
 
@@ -28,19 +28,42 @@ namespace Utils {
   export type Pop<T extends unknown[]> = T extends [...infer Head, infer _]
     ? Head
     : [];
+
+  export type MapNums<T extends [...string[]]> = {
+    [K in keyof T]: Utils.ToNumber<T[K]>;
+  };
+
+  export type Digits<T extends NumLike> = MapNums<Split<`${T}`>>;
+  type xxxxx = Digits<289328>;
+  //   ^?
+
+  export type Join<T extends number[]> = T extends [
+    infer Head extends number,
+    ...infer Rest extends number[]
+  ]
+    ? `${Head}${Join<Rest>}`
+    : "";
+
+  export type Split<S extends string> = S extends `${infer Head}${infer Rest}`
+    ? [Head, ...Split<Rest>]
+    : [];
+
+  export type And<T, U> = T extends true
+    ? U extends true
+      ? true
+      : false
+    : false;
 }
 
+type IsZero<T extends number> = T extends 0 ? true : false;
 type End<
   Num1 extends number[],
   Num2 extends number[],
   Carry extends 0 | 1 = 0
-> = Num1["length"] extends 0
-  ? Num2["length"] extends 0
-    ? Carry extends 0
-      ? true
-      : false
-    : false
-  : false;
+> = Utils.And<
+  Utils.And<IsZero<Num1["length"]>, IsZero<Num2["length"]>>,
+  IsZero<Carry>
+>;
 
 type Adder<
   Num1 extends number[],
@@ -55,37 +78,24 @@ type Adder<
   ? []
   : [...Adder<Utils.Pop<Num1>, Utils.Pop<Num2>, GetCarry<Sum>>, GetDigit<Sum>];
 
-type Split<S extends string> = S extends `${infer Head}${infer Rest}`
-  ? [Head, ...Split<Rest>]
-  : [];
-
 type GetCarry<T extends number> =
   `${T}` extends `${infer C}${infer _}${infer _}` ? Utils.ToNumber<C> : 0;
-type GetDigit<T extends number> = Utils.ToNumber<Utils.Last<Split<`${T}`>>>;
+type GetDigit<T extends number> = Utils.ToNumber<
+  Utils.Last<Utils.Split<`${T}`>>
+>;
 
 type NumLike = number | bigint | string;
 
-type MapNums<T extends [...string[]]> = {
-  [K in keyof T]: Utils.ToNumber<T[K]>;
-};
-type Digits<T extends NumLike> = MapNums<Split<`${T}`>>;
-
-type Join<T extends number[]> = T extends [
-  infer Head extends number,
-  ...infer Rest extends number[]
-]
-  ? `${Head}${Join<Rest>}`
-  : "";
-
 export type Add<M extends NumLike, N extends NumLike> = Adder<
-  Digits<M>,
-  Digits<N>
+  Utils.Digits<M>,
+  Utils.Digits<N>
 > extends infer R extends number[]
-  ? Utils.ToNumber<Join<R>>
+  ? Utils.ToNumber<Utils.Join<R>>
   : never;
 
 type zzzzzz = Add<234239, 234230004>;
 //    ^?
-
-type xxxxx = Digits<289328>;
-//   ^?
+type aaaaaa = Add<9, 12>;
+//    ^?
+type bbbbbb = Add<990, 12>;
+//    ^?
